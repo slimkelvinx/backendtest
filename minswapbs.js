470b3e769efd7435c12937b81faa3a5a2a61600b
slimkelvinx <50533174+slimkelvinx@users.noreply.github.com>
2019-07-12 21:42:48 -0700

    Update minswapbs.js

--- minswapbs.js
+++ minswapbs.js
@@ -1,12 +1,12 @@
-function minimumSwaps(arr) {
+const minimumSwaps = (arr) => {
 
-   var counter = 0;
+   let counter = 0;
 
-   for (var i = arr.length; i > 0; i--) {
-      var minval = Math.min(...arr); console.log("before", arr);
-      var minIndex = arr.indexOf(minval);
+   for (let i = arr.length; i > 0; i--) {
+      let minval = Math.min(...arr); console.log("before", arr);
+      let minIndex = arr.indexOf(minval);
       if (minval != = arr[0]) {
-         var temp = arr[0];
+         let temp = arr[0];
          arr[0] = arr[minIndex];
          arr[minIndex] = temp; console.log("after", arr);
          arr.splice(0, 1);
